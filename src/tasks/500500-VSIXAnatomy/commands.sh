112373040e840b638bb1db156b003ddad85a8684
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-04-22 09:11:41 +0530

    Added more references

--- src/tasks/500500-VSIXAnatomy/commands.sh
+++ src/tasks/500500-VSIXAnatomy/commands.sh
@@ -1,8 +1,23 @@
 cd ../../..
 
-cd src/tasks/500500
+cd src/tasks/500500-VSIXAnatomy
 
-cd src/apps/500500
+cd src/apps/500500-VSIXAnatomy
 
-dotnet run --project ./VSIXAnatomy.csproj
+## I dont think we can build using the following command. 
+## The project is not a dotnet core project. 
+
+dotnet build --project ./VSIXAnatomy.csproj
+
+# For the following command to work, devenv must be added to the path environment variable. 
+# First ensure you have visual studio is installed on your machine.
+# On my macine, the devenv exe is available at the following location.
+# C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE
+# Next add this to the path evnironment variable. 
+
+# https://stackoverflow.com/a/76077904/1977871
+
+devenv /build Debug ./VSIXAnatomy.sln
+
+
 
