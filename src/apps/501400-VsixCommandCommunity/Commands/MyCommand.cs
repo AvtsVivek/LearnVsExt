1e342a0de3cffea9100d4ab4f0fee54fb22055b5
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-04-28 15:26:37 +0530

    Adde new project

---
+++ src/apps/501400-VsixCommandCommunity/Commands/MyCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace VsixCommandCommunity
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("VsixCommandCommunity", "Button clicked");
+        }
+    }
+}
---
+++ src/tasks/501400-VsixCommandCommunity/ReadMe.md
@@ -0,0 +1,12 @@
+
+Use the following.
+
+![Visual Studio Command Community Project](./images/50VSixCommandCommunityProject50.jpg)
+
+Once installed, my command.
+
+![My Command Visual Studio](./images/55MyCommand50.jpg)
+
+Solution Explorer.
+
+![Solution Explorer](./images/57SolutionExplorer50.jpg)
