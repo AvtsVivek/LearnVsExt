110a321345eeeb40ec0f7635d0b8fc5d35e55859
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-05-11 17:12:09 +0530

    Renamed the projects - corrections

--- src/tasks/552400-VsixCommandCommunity/commands.sh
+++ src/tasks/552400-VsixCommandCommunity/commands.sh
@@ -1,8 +1,8 @@
 cd ../../..
 
-cd src/tasks/502400-VsixCommandCommunity
+cd src/tasks/552400-VsixCommandCommunity
 
-cd src/apps/502400-VsixCommandCommunity
+cd src/apps/552400-VsixCommandCommunity
 
 ## I dont think we can build using the following dotnet command. 
 ## The project is not a dotnet core project. 
