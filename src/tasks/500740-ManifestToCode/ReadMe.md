fcabd89e5347257fd8ced0ac2060fa6ee42d39d2
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-05-31 12:45:57 +0530

    Added more info

--- src/tasks/500740-ManifestToCode/ReadMe.md
+++ src/tasks/500740-ManifestToCode/ReadMe.md
@@ -7,11 +7,18 @@
 
 2. Follow the steps from earlier example. Look at the earlier example [500735-ManifestFromResources](https://github.com/AvtsVivek/LearnVsExt/tree/main/src/tasks/500735-ManifestFromResources)
 
-6. Run the following command to create a manifest file. 
+3. Ensure you are in the folder src/apps/500740-ManifestToCode. Run the following command to create a manifest file. 
 
 ```cmd
 ManifestFromResources /resources:C:/Trials/Ex/LearnVsExt/src/apps/500740-ManifestToCode/images/Save.png /assembly:ManifestToCodeAssembly /manifest:MyImageManifest.imagemanifest
 ```
 
-7. The above command should create a file by the name MyImageManifest.imagemanifest in the folder src/apps/500735-ManifestFromResources
+4. The above command should create a file by the name MyImageManifest.imagemanifest in the folder src/apps/500740-ManifestToCode
 
+5. Next run the following command to generate cs files from the manifest file.
+
+```cmd
+ManifestToCode /manifest:MyImageManifest.imagemanifest /language:CSharp /namespace:ManifestToCode /imageIdClass:MyImageIds /monikerClass:MyMonikers /classAccess:public
+```
+
+6. The above command generats the cs files. Next include files in the project. 
--- src/tasks/500740-ManifestToCode/commands.sh
+++ src/tasks/500740-ManifestToCode/commands.sh
@@ -10,23 +10,5 @@ ManifestFromResources /resources:C:/Trials/Ex/LearnVsExt/src/apps/500740-Manifes
 # The above command should create a file by the name MyImageManifest.imagemanifest in the folder src/apps/500740-ManifestToCode
 
 # Now run the following command. The difference from the above is the assembly. Earlier it was ManifestFromResourceAssembly. Now it is ResourceAssembly 
-ManifestToCode /manifest:MyImageManifest.imagemanifest /language:CSharp
-ManifestFromResources /resources:C:/Trials/Ex/LearnVsExt/src/apps/500735-ManifestFromResources/images/Save.png /assembly:ResourceAssembly /manifest:MyImageManifest.imagemanifest
+ManifestToCode /manifest:MyImageManifest.imagemanifest /language:CSharp /namespace:ManifestToCode /imageIdClass:MyImageIds /monikerClass:MyMonikers /classAccess:public
 
-# Now observe the difference in the file MyImageManifest.imagemanifest.
-
-# Now add one more file. 
-# Run the command once more.
-
-ManifestFromResources /assembly:ResourceAssembly /manifest:MyImageManifest.imagemanifest /resources:"C:/Trials/Ex/LearnVsExt/src/apps/500735-ManifestFromResources/images/Save.png;C:/Trials/Ex/LearnVsExt/src/apps/500735-ManifestFromResources/images/GitHub.png" 
-
-# Now observe the imagemanifest file. GitHub.png is added.
-
-```xml
-  <ItemGroup>
-    <Content Include="images\GitHub.png" />
-    <Content Include="images\Save.png" />
-  </ItemGroup>
-```
-
-ManifestToCode /manifest:D:\MyManifest.imagemanifest /language:CSharp
