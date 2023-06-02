1e5ac6fd51b4b06565464442c401da65a94ddaa4
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-06-02 17:18:05 +0530

    Added more info

--- src/tasks/500740-ManifestToCode/commands.sh
+++ src/tasks/500740-ManifestToCode/commands.sh
@@ -8,6 +8,7 @@ cd src/apps/500740-ManifestToCode
 ManifestFromResources /resources:C:/Trials/Ex/LearnVsExt/src/apps/500740-ManifestToCode/images/Save.png /assembly:ManifestToCodeAssembly /manifest:MyImageManifest.imagemanifest
 # Or you can use the following as well.
 ManifestFromResources /resources:./images/Save.png /assembly:ManifestToCodeAssembly /manifest:MyImageManifest.imagemanifest
+ManifestFromResources /resources:C:/Trials/Ex/LearnVsExt/src/apps/500740-ManifestToCode/images/ /assembly:C:/Trials/Ex/LearnVsExt/src/apps/500740-ManifestToCode/bin/Debug/ManifestToCode /manifest:MyImageManifest.imagemanifest
 
 # The above command should create a file by the name MyImageManifest.imagemanifest in the folder src/apps/500740-ManifestToCode
 
