3a2c3fa33ccff8d29315a30c62a0811c0fc8d9c5
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-06-08 11:39:43 +0530

    Removed comments

--- src/apps/501145-SimpleWebSearch/Commands/SimpleWebSearchPackage.cs
+++ src/apps/501145-SimpleWebSearch/Commands/SimpleWebSearchPackage.cs
@@ -17,9 +17,6 @@ namespace SimpleWebSearch.Commands
 
         public const string guidSimpleWebSearchPackageCmdSetString = "da587d54-32dc-4e4f-b762-2cd5abe48811";
         public static Guid guidSimpleWebSearchPackageCmdSet = new Guid(guidSimpleWebSearchPackageCmdSetString);
-
-        public const string guidImagesString = "3e8b16b0-1490-492b-aa69-3354046a3cf0";
-        public static Guid guidImages = new Guid(guidImagesString);
     }
     /// <summary>
     /// Helper class that encapsulates all CommandIDs uses across VS Package.
@@ -28,11 +25,5 @@ namespace SimpleWebSearch.Commands
     {
         public const int MyMenuGroup = 0x1020;
         public const int FindOnWebCommandId = 0x0100;
-        public const int bmpPic1 = 0x0001;
-        public const int bmpPic2 = 0x0002;
-        public const int bmpPicSearch = 0x0003;
-        public const int bmpPicX = 0x0004;
-        public const int bmpPicArrows = 0x0005;
-        public const int bmpPicStrikethrough = 0x0006;
     }
 }
