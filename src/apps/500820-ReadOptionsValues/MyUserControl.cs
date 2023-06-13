d6019000ecde19d2fb2252781a1cc4cb8812c947
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-06-13 16:41:42 +0530

    Added new project

---
+++ src/apps/500820-ReadOptionsValues/MyUserControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadOptionsValues
+{
+    public partial class MyUserControl : UserControl
+    {
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+        internal OptionPageCustom optionsPage;
+
+        public void Initialize()
+        {
+            textBox1.Text = optionsPage.OptionString;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            optionsPage.OptionString = textBox1.Text;
+        }
+    }
+}
