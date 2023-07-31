fe3329d3770d982040862aa50d00bd919c456a30
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-07-31 18:02:29 +0530

    Added new project

---
+++ src/apps/580500-WebSocketExOne/WebSocketExOneClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+
+Console.Title = "Client";
+
+using var ws = new ClientWebSocket();
+
+await ws.ConnectAsync(new Uri("ws://localhost:5050/ws"), CancellationToken.None);
+byte[] buf = new byte[1056];
+
+while (ws.State == WebSocketState.Open)
+{
+    var result = await ws.ReceiveAsync(buf, CancellationToken.None);
+
+    if (result.MessageType == WebSocketMessageType.Close)
+    {
+        await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
+        Console.WriteLine(result.CloseStatusDescription);
+    }
+    else
+    {
+        Console.WriteLine(Encoding.ASCII.GetString(buf, 0, result.Count));
+    }
+}
