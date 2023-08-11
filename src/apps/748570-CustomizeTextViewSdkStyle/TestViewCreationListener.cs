7f580bb7f59ae0576fba064048e9cbd8b98fe26b
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-08-11 15:57:28 +0530

    Added more info

---
+++ src/apps/748570-CustomizeTextViewSdkStyle/TestViewCreationListener.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Media;
+using System.Windows;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace CustomizeTextViewSdkStyle
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    // [ContentType("code")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal class TestViewCreationListener : IWpfTextViewCreationListener
+    {
+        [Import]
+        internal IEditorFormatMapService FormatMapService = null;
+
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            IEditorFormatMap formatMap = FormatMapService.GetEditorFormatMap(textView);
+
+            ResourceDictionary regularCaretProperties = formatMap.GetProperties("Caret");
+            ResourceDictionary overwriteCaretProperties = formatMap.GetProperties("Overwrite Caret");
+            ResourceDictionary indicatorMargin = formatMap.GetProperties("Indicator Margin");
+            ResourceDictionary visibleWhitespace = formatMap.GetProperties("Visible Whitespace");
+            ResourceDictionary selectedText = formatMap.GetProperties("Selected Text");
+            ResourceDictionary inactiveSelectedText = formatMap.GetProperties("Inactive Selected Text");
+
+            formatMap.BeginBatchUpdate();
+
+            regularCaretProperties[EditorFormatDefinition.ForegroundBrushId] = Brushes.Magenta;
+            formatMap.SetProperties("Caret", regularCaretProperties);
+
+            overwriteCaretProperties[EditorFormatDefinition.ForegroundBrushId] = Brushes.Turquoise;
+            formatMap.SetProperties("Overwrite Caret", overwriteCaretProperties);
+
+            indicatorMargin[EditorFormatDefinition.BackgroundColorId] = Colors.LightGreen;
+            formatMap.SetProperties("Indicator Margin", indicatorMargin);
+
+            visibleWhitespace[EditorFormatDefinition.ForegroundColorId] = Colors.Red;
+            formatMap.SetProperties("Visible Whitespace", visibleWhitespace);
+
+            selectedText[EditorFormatDefinition.BackgroundBrushId] = Brushes.LightPink;
+            formatMap.SetProperties("Selected Text", selectedText);
+
+            inactiveSelectedText[EditorFormatDefinition.BackgroundBrushId] = Brushes.DeepPink;
+            formatMap.SetProperties("Inactive Selected Text", inactiveSelectedText);
+
+            formatMap.EndBatchUpdate();
+        }
+    }
+
+}
---
+++ src/tasks/748570-CustomizeTextViewSdkStyle/ReadMe.md
@@ -0,0 +1,37 @@
+# Customizing the text view
+
+## Reference: 
+1. https://learn.microsoft.com/en-us/visualstudio/extensibility/walkthrough-customizing-the-text-view
+
+## How this project is created. 
+1. Create a VSix project.
+
+3. Then add the class **TestViewCreationListener**.
+
+4. Build and run. 
+
+## Notes
+
+1. Once the exp instance is launched, open a text(TextFile1.txt for example) file. It has a text "asdfasdf".
+
+![Open Text File](images/52_50OpeningATextFile.jpg)
+
+2. Select that text and then click on the solution explorer. You will see that the selection gets pink. 
+
+3. Now, click on the code window, ensure no text is selected, then Edit -> Advanced -> View White Space.
+
+![View White Spaces](images/53_50EditAdvanced.jpg)
+
+4. Now press some tabs. You should now see them as red arrows.
+
+![Tabs Shown as red arrows](images/54_50TabsShownAsRedArrows.jpg)
+
+5. So this is working with text file. Note the ContentType attribute with "text" as parameter. 
+
+6. I tried with "code", "CSharp" etc. Nothing worked. Not sure why.  
+
+```cs
+[ContentType("code")]
+```
+
+7. 
