9c8133326b6518a1059f7ea03d4bad4c2c676f17
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-08-30 15:47:42 +0530

    Added new project

--- src/apps/805500-SimpleVisualizerCustomDataObject/MyFirstVisualizer/DebuggerSide.cs
+++ src/apps/805500-SimpleVisualizerCustomDataObject/MyFirstVisualizer/DebuggerSide.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.DebuggerVisualizers;
+using System.Runtime.InteropServices.ComTypes;
 using System.Windows.Forms;
 
 
@@ -11,7 +12,14 @@ namespace MyFirstVisualizer
     {
         protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
         {
-            MessageBox.Show(objectProvider.GetObject().ToString());
+            // MessageBox.Show(objectProvider.GetObject().ToString());
+
+            var data = objectProvider.GetObject() as MyDataObject.CustomDataObject;
+
+            // You can replace displayForm with your own custom Form or Control.  
+            Form displayForm = new Form();
+            displayForm.Text = data.MyData;
+            windowService.ShowDialog(displayForm);
         }
 
         public static void TestShowVisualizer(object objectToVisualize)
---
+++ src/apps/805500-SimpleVisualizerCustomDataObject/MyTestConsole/TestConsole.cs
@@ -0,0 +1,15 @@
+﻿using MyDataObject;
+using MyFirstVisualizer;
+
+namespace MyTestConsole
+{
+    internal class TestConsole
+    {
+        static void Main(string[] args)
+        {
+            var customDataObject = new CustomDataObject();
+
+            DebuggerSide.TestShowVisualizer(customDataObject);
+        }
+    }
+}
