64f0f8ff4ed9b0822f042c2b1f8759c25adf0f4d
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-08-30 15:11:19 +0530

    Added new project

--- src/tasks/805500-SimpleVisualizerCustomDataObject/commands.sh
+++ src/tasks/805500-SimpleVisualizerCustomDataObject/commands.sh
@@ -1,13 +1,13 @@
 cd ../../..
 
-cd src/tasks/801100-CaretFishEye
+cd src/tasks/805500-SimpleVisualizerCustomDataObject
 
-cd src/apps/801100-CaretFishEye
+cd src/apps/805500-SimpleVisualizerCustomDataObject
 
 ## I dont think we can build using the following dotnet command. 
 ## The project is not a dotnet core project. 
 
-dotnet build --project ./CaretFishEye.csproj
+dotnet build --project ./SimpleVisualizerCustomDataObject.csproj
 
 # For the following command to work, devenv must be added to the path environment variable. 
 # First ensure you have visual studio is installed on your machine.
@@ -19,10 +19,10 @@ dotnet build --project ./CaretFishEye.csproj
 
 # The following build commands does not seem to work. 
 # For debugging, build using visual studio only!!!
-devenv /build Debug ./CaretFishEye.sln
+devenv /build Debug ./SimpleVisualizerCustomDataObject.sln
 
 # If you just want to start and run visual studi in experimental mode, run the following command.
-devenv.exe /RootSuffix Exp ./CaretFishEye.sln
+devenv.exe /RootSuffix Exp ./SimpleVisualizerCustomDataObject.sln
 
 pwd
 
@@ -35,7 +35,7 @@ Get-ChildItem
 # Now to install the extension, first ensure all the instances of Visual Studio are closed.
 # Now simply run the following command to install the extension
 
-./CaretFishEye.vsix
+./SimpleVisualizerCustomDataObject.vsix
 
 
 # Once installed, open the logs. You will see something like. 
