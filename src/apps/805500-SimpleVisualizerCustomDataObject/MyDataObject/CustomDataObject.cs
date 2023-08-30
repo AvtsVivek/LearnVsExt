58af9de050aff40892c162a53c4879db967acc5c
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-08-30 15:03:34 +0530

    Added new project

---
+++ src/apps/805500-SimpleVisualizerCustomDataObject/MyDataObject/CustomDataObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDataObject
+{
+    [Serializable]
+    public class CustomDataObject
+    {
+        public CustomDataObject()
+        {
+            this.MyData = "MyTestData";
+        }
+        public string MyData { get; set; }
+    }
+}
---
+++ src/apps/805500-SimpleVisualizerCustomDataObject/MyTestConsole/TestConsole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyDataObject;
+using MyFirstVisualizer;
+
+namespace MyTestConsole
+{
+    internal class TestConsole
+    {
+        static void Main(string[] args)
+        {
+            // var myString = "Hello, World";
+            // DebuggerSide.TestShowVisualizer(myString);
+
+            var customDataObject = new CustomDataObject();
+
+            DebuggerSide.TestShowVisualizer(customDataObject);
+        }
+    }
+}
