8dabdcdc5e3a5a5387353d7a94259ae8324c6985
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2023-10-20 19:04:40 +0530

    Added more info

---
+++ src/apps/903950-SolutionEventsIntro/SolutionEventsIntroPackage.cs
@@ -0,0 +1,143 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using Task = System.Threading.Tasks.Task;
+
+namespace SolutionEventsIntro
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string, PackageAutoLoadFlags.BackgroundLoad)]
+    public sealed class SolutionEventsIntroPackage : AsyncPackage
+    {
+        /// <summary>
+        /// SolutionEventsIntroPackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "9dca66b7-52d6-454c-86b0-1a3acba1b82d";
+
+        public SolutionEvents SolutionEventsInstance { 
+            [DispId(302)] 
+            get;
+            set;
+        }
+
+        public static DTE2 DteTwoInstance
+        {
+            get;
+            private set;
+        }
+
+        public SolutionEventsIntroPackage()
+        {          
+
+        }
+
+
+        private void SolutionEvents_AfterClosing()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            MessageBox.Show("After Closing");
+        }
+
+        private void SolutionEvents_BeforeClosing()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            MessageBox.Show("Before Closing");
+        }
+
+        private void SolutionEvents_Opened()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            MessageBox.Show("Opened");
+        }
+
+        private void SolutionEvents_ProjectAdded(Project Project)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            MessageBox.Show("Project Added");
+        }
+
+        private void SolutionEvents_ProjectRemoved(Project Project)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            MessageBox.Show("Project Removed");
+        }
+        private void SolutionEvents_ProjectRenamed(Project Project, string OldName)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            MessageBox.Show("Project Renamed");
+        }
+
+        private void SolutionEvents_QueryCloseSolution(ref bool fCancel)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            MessageBox.Show("Query Close Solution");
+        }
+
+        private void SolutionEvents_Renamed(string OldName)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            MessageBox.Show("Renamed");
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            // ThreadHelper.ThrowIfNotOnUIThread();
+
+            DteTwoInstance = await GetServiceAsync(typeof(DTE)) as DTE2;
+
+            SolutionEventsInstance = DteTwoInstance.Events.SolutionEvents;
+            // bldevents = CType(applicationObject.Events.BuildEvents, EnvDTE.BuildEvents)
+
+            SolutionEventsInstance.AfterClosing += SolutionEvents_AfterClosing;
+            SolutionEventsInstance.BeforeClosing += SolutionEvents_BeforeClosing;
+            SolutionEventsInstance.Opened += SolutionEvents_Opened;
+            SolutionEventsInstance.ProjectAdded += SolutionEvents_ProjectAdded;
+            SolutionEventsInstance.ProjectRemoved += SolutionEvents_ProjectRemoved;
+            SolutionEventsInstance.ProjectRenamed += SolutionEvents_ProjectRenamed;
+            SolutionEventsInstance.QueryCloseSolution += SolutionEvents_QueryCloseSolution;
+            SolutionEventsInstance.Renamed += SolutionEvents_Renamed;
+
+            // await Commands.Command1.InitializeAsync(this);
+        }
+
+        #endregion
+    }
+}
--- src/tasks/501125-ProvideAutoLoad/ReadMe.md
+++ src/tasks/501125-ProvideAutoLoad/ReadMe.md
@@ -1,20 +1,20 @@
 ## Objective
 
-1. This builds on [task](https://github.com/AvtsVivek/LearnVsExt/tree/main/src/tasks/501100-AddMenuVsMainMenuBar) and [app](https://github.com/AvtsVivek/LearnVsExt/tree/main/src/apps/501100-AddMenuVsMainMenuBar).  
+1. This builds on an earlier example [501100-AddMenuVsMainMenuBar].  
 
 2. This example adds and demos [ProvideAutoLoad attribute](https://learn.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.shell.provideautoloadattribute). 
 
-3. References
-   1. https://learn.microsoft.com/en-us/visualstudio/extensibility/how-to-use-asyncpackage-to-load-vspackages-in-the-background#create-an-asyncpackage
+## References
+1. https://learn.microsoft.com/en-us/visualstudio/extensibility/how-to-use-asyncpackage-to-load-vspackages-in-the-background#create-an-asyncpackage
 
-   2. https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-2017/extensibility/how-to-use-asyncpackage-to-load-vspackages-in-the-background?view=vs-2017#create-an-asyncpackage
+2. https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-2017/extensibility/how-to-use-asyncpackage-to-load-vspackages-in-the-background?view=vs-2017#create-an-asyncpackage
    
-   3. https://learn.microsoft.com/en-us/visualstudio/extensibility/adding-a-menu-to-the-visual-studio-menu-bar
+3. https://learn.microsoft.com/en-us/visualstudio/extensibility/adding-a-menu-to-the-visual-studio-menu-bar
    
-   4. https://www.youtube.com/watch?v=p328QcgZObs&t=526s
+4. https://www.youtube.com/watch?v=p328QcgZObs&t=526s
 
-4. Asdf
 
+## Notes
 ```cs
 [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
 [Guid(ProvideAutoLoadPackage.PackageGuidString)]
@@ -27,21 +27,21 @@ public sealed class ProvideAutoLoadPackage : AsyncPackage
 }
 ``` 
 
-5. Consider the attributes applied above. Take a look at the YouTube ref: https://www.youtube.com/watch?v=p328QcgZObs&t=526s
+1. Consider the attributes applied above. Take a look at the YouTube ref: https://www.youtube.com/watch?v=p328QcgZObs&t=526s
 ```cs
 [ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string, PackageAutoLoadFlags.BackgroundLoad)]
 [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string, PackageAutoLoadFlags.BackgroundLoad)]
 ```
 
-   So they are not events. Think of them as states. https://www.youtube.com/watch?v=p328QcgZObs&t=565s
+2. So they are not events. Think of them as states. https://www.youtube.com/watch?v=p328QcgZObs&t=565s
 
-6. Try running debugging this app with and without the above attributes. Each time you run the app, ensure to reset the exp vs as follows.
+3. Try running debugging this app with and without the above attributes. Each time you run the app, ensure to reset the exp vs as follows.
 
-![Reset Visual Studio Exp](https://github.com/AvtsVivek/LearnVsExt/blob/main/src/tasks/400500-VSixBlankProjectAnalysis/images/110ResetVsExpIntance50.jpg)
+![Reset Visual Studio Exp](../400500-VSixBlankProjectAnalysis/images/110ResetVsExpIntance50.jpg)
 
-Visual studio doesn’t load the Command until first use. Which means, our Command’s constructor of the package won’t be called.
+4. Visual studio doesn’t load the Command until first use. Which means, our Command’s constructor of the package won’t be called.
 
-If we need to make our Command to initialize at Visual Studio startup, that is, if we need our ctor to be called at start up, we need to do a bit more. To do that, we need to make our VSPackage to initialize at startup. This is done with the ProvideAutoLoad attribute in the package class file.
+5. If we need to make our Command to initialize at Visual Studio startup, that is, if we need our ctor to be called at start up, we need to do a bit more. To do that, we need to make our VSPackage to initialize at startup. This is done with the ProvideAutoLoad attribute in the package class file.
 
 ```cs
 public ProvideAutoLoadPackage()
@@ -59,13 +59,13 @@ protected override async Task InitializeAsync(CancellationToken cancellationToke
 }
 ```
 
-So when you have the attributes, then the above two break points are hit during the start of the visual studio when the packages are loaded. The package is loaded when the visual studio starts.
+6. So when you have the attributes, then the above two break points are hit during the start of the visual studio when the packages are loaded. The package is loaded when the visual studio starts.
 
-This is not very efficient, because the package is loaded and the command may never be invoked at all.
+7. This is not very efficient, because the package is loaded and the command may never be invoked at all.
 
-But when you comment out the attributes, then the above two break points are hit when the command is invoked. The package is loaded when the command is invoked. The package is loaded NOT when the visual studio starts.  
+8. But when you comment out the attributes, then the above two break points are hit when the command is invoked. The package is loaded when the command is invoked. The package is loaded NOT when the visual studio starts.  
 
-We have the attribute twice: When there’s no Solution, and when Solution exists. Which covers all cases. Also, note the PackageAutoLoadFlags.BackgroundLoad flag. This important flag states that our packages can initialize asynchronously on a background thread.
+9. We have the attribute twice: When there’s no Solution, and when Solution exists. Which covers all cases. Also, note the PackageAutoLoadFlags.BackgroundLoad flag. This important flag states that our packages can initialize asynchronously on a background thread.
 
 
 
