d46d946921c7a982208c8dc37ae0e11f55f5e241
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2024-12-04 13:24:11 +0530

    added new proj

---
+++ src/apps/221500-TextBufferIntro/TextBufferIntro/BasicTextManipulationControl.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.VisualStudio.ComponentModelHost;
+
+namespace TextBufferIntro
+{
+    /// <summary>
+    /// Interaction logic for BasicTextManipulationControl.
+    /// </summary>
+    public partial class BasicTextManipulationControl : UserControl
+    {
+        private ITextBufferFactoryService _textBufferFactoryService = null;
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private static AsyncPackage _package;
+
+        internal static void Initialize(AsyncPackage package)
+        {
+            _package = package;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BasicTextManipulationControl"/> class.
+        /// </summary>
+        public BasicTextManipulationControl()
+        {
+            this.InitializeComponent();
+            
+            var componentModel = (IComponentModel)Package.GetGlobalService(typeof(SComponentModel));
+
+            _textBufferFactoryService = componentModel.GetService<ITextBufferFactoryService>();
+
+            if (_textBufferFactoryService == null)
+                throw new Exception($"{nameof(_textBufferFactoryService)} is null. Cannot continue!!!");
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var text = textBox1.Text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                MessageBox.Show("Enter some valid text", "No text entered", MessageBoxButton.OK, 
+                    MessageBoxImage.Exclamation);
+                return;
+            }
+
+            // Create the text buffer
+            var textBuffer = _textBufferFactoryService.CreateTextBuffer(text, contentType: _textBufferFactoryService.PlaintextContentType);
+
+            var snapshot = textBuffer.CurrentSnapshot;
+
+            var resultNumberSubString = Regex.Match(snapshot.GetText(), @"\d+").Value;
+
+            if (string.IsNullOrEmpty(resultNumberSubString))
+            {
+                MessageBox.Show("Text does not contain number", "No number", MessageBoxButton.OK, 
+                    MessageBoxImage.Exclamation);
+                return;
+            }
+
+            var resultNumberSubStringIndex = snapshot.GetText().IndexOf(resultNumberSubString, 0, 
+                snapshot.GetText().Length, StringComparison.CurrentCulture);
+
+            var numberSpan = new SnapshotSpan(snapshot, span: new Span(start: resultNumberSubStringIndex,
+               length: resultNumberSubString.Length));
+
+            finalNumberText.Text = numberSpan.GetText();
+        }
+    }
+}
---
+++ src/tasks/221500-TextBufferIntro/ReadMe.md
@@ -0,0 +1,25 @@
+## Objective
+
+1. Introduces `ITextBuffer`, `ITextBufferFactoryService`, `ITextSnapshot`, `SnapshotSpan` and `Span`
+
+2. For the full article, [click here](1-ITextBuffer.md)
+
+## Notes
+
+1. A [Microsoft.Visualstudio.Text.Span](https://learn.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.text.span) is a very simple concept. Its a struct.
+
+2. An immutable integer interval that describes a range of values from Start to End that is closed on the left and open on the right: [Start .. End). A span is usually applied to an [ITextSnapshot](https://learn.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.text.itextsnapshot) to denote a span of text, but it is independent of any particular text buffer or snapshot.
+
+3. Its simply a number range. Thats all. The problem with using such types (Span and Simple Int) is that they are detached from the specific text, and if we have calculated the position or interval for one snapshot, they may indicate something completely different or be invalid for another. In other words, a Position or Span only makes sense in the *context* of a particular [ITextSnapshot](https://learn.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.text.itextsnapshot). 
+
+4. Therefore, the following two types have been added:
+
+   1. [SnapshotPoint](https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.text.snapshotpoint)
+   2. [SnapshotSpan](https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.text.snapshotspan)
+
+Essentially, these are structures that contain the position (or interval) and the pointer to the Snapshot for which they were obtained. In addition, they implement methods of comparison and manipulation (for example, to shift a position by a given distance) with a check for admissibility (for example, for not going beyond the border of the Snapshot).
+
+5. 
+
+## Reference.
+1. https://mihailromanov.wordpress.com/2021/11/05/json-on-steroids-2-2-visual-studio-editor-itextbuffer-and-related-types
