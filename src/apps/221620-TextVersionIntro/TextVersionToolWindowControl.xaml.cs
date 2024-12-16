460702e406c51db7f0dc9ae2ccb3ab20dcae956a
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2024-12-16 14:22:36 +0530

    Added more info

---
+++ src/apps/221620-TextVersionIntro/TextVersionToolWindowControl.xaml.cs
@@ -0,0 +1,413 @@
+﻿using EnvDTE80;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Package;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Web.UI.Design;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace TextVersionIntro
+{
+    /// <summary>
+    /// Interaction logic for TextVersionToolWindowControl.
+    /// </summary>
+    public partial class TextVersionToolWindowControl : UserControl
+    {
+        private ITextBufferFactoryService _textBufferFactoryService = null;
+
+        private ITextBuffer _textBuffer = null;
+
+        private ITextEdit _textEdit = null;
+
+        private List<OperationData> _textOperationList;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextVersionToolWindowControl"/> class.
+        /// </summary>
+        public TextVersionToolWindowControl()
+        {
+            this.InitializeComponent();
+
+            var componentModel = (IComponentModel)Package.GetGlobalService(typeof(SComponentModel));
+
+            _textBufferFactoryService = componentModel.GetService<ITextBufferFactoryService>();
+
+            if (_textBufferFactoryService == null)
+                throw new Exception($"{nameof(_textBufferFactoryService)} is null. Cannot continue!!!");
+
+            _textOperationList = new List<OperationData>();
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "TextVersionToolWindow");
+        }
+
+        private void ITextEditReplaceButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsTextBufferNull())
+            {
+                return;
+            }
+
+            var inputText = ITextEditInputTextBox.Text;
+
+            if (string.IsNullOrEmpty(inputText))
+            {
+                MessageBox.Show(
+                    messageBoxText: "Source text of ITextEdit Manipulationi for Insert is empty. Please input some text",
+                    caption: "No input");
+            }
+
+            if (!TryParseTextBoxToInt(textBox: ITextEditInputPositionReplaceTxtBox, textBoxValue: out int position))
+            {
+                MessageBox.Show(
+                messageBoxText: "Position int is null. Cannot continue",
+                caption: "No input");
+                return;
+            }
+
+            if (!TryParseTextBoxToInt(textBox: ITextEditInputLengthReplaceTxtBox, textBoxValue: out int length))
+            {
+                MessageBox.Show(
+                messageBoxText: "Length int is null. Cannot continue",
+                caption: "No input");
+
+                return;
+            }
+
+            var replaceString = string.Empty;
+            if (!string.IsNullOrEmpty(value: ITextEditInputTextReplaceTxtBox.Text))
+            {
+                replaceString = ITextEditInputTextReplaceTxtBox.Text;
+            }
+
+            if (!_textBuffer.EditInProgress)
+            {
+                _textEdit = _textBuffer.CreateEdit();
+            }
+
+            _textEdit.Replace(startPosition: position, charsToReplace: length, replaceWith: replaceString);
+
+            PublishCurrentSnapshotAfterOperation();
+
+            _textOperationList.Add(new OperationData { Position = position, Length = length, Operation = TextOperation.Replace, OperationText = replaceString });
+
+        }
+
+        private void ITextEditInsertButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsTextBufferNull())
+            {
+                return;
+            }
+
+            var inputText = ITextEditInputTextBox.Text;
+
+            if (string.IsNullOrEmpty(inputText))
+            {
+                MessageBox.Show(messageBoxText: "Source text of ITextEdit Manipulation for Insert is empty. Please input some text",
+                    caption: "No input");
+            }
+
+            if (!TryParseTextBoxToInt(textBox: ITextEditInputPositionInsertTxtBox, textBoxValue: out int position))
+            {
+                MessageBox.Show(
+                messageBoxText: "Position int is null. Cannot continue",
+                caption: "No input");
+                return;
+            }
+
+            var insertString = string.Empty;
+            if (!string.IsNullOrEmpty(value: ITextEditInputTextReplaceTxtBox.Text))
+            {
+                insertString = ITextEditInputTextReplaceTxtBox.Text;
+            }
+
+            if (!_textBuffer.EditInProgress)
+            {
+                _textEdit = _textBuffer.CreateEdit();
+            }
+
+            // _textEdit.Replace(startPosition: position, charsToReplace: length, replaceWith: replaceString);
+
+
+
+            try
+            {
+                _textEdit.Insert(position, ITextEditInputTextInsertTxtBox.Text);
+            }
+            catch (Exception exception)
+            {
+                if (exception.Message == "Attempted to reuse an already applied edit.")
+                {
+                    MessageBox.Show(messageBoxText: $"{exception.Message} So restart by clicking the Reset button, then start and then the opration insert, delete or reset. " +
+                        $"{Environment.NewLine}Click the Apply button at the end.",
+                    caption: "Start again");
+                    return;
+                }
+            }
+
+            PublishCurrentSnapshotAfterOperation();
+
+            _textOperationList.Add(new OperationData { Position = position, Operation = TextOperation.Insert, OperationText = insertString });
+
+
+            // ITextEditApply();
+            // AddItemToListView(startPosition: position, 0, operationText: ITextEditInputTextInsertTxtBox.Text, operation: "Insert");
+        }
+
+        private void ITextEditDeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsTextBufferNull())
+            {
+                return;
+            }
+
+            var inputText = ITextEditInputTextBox.Text;
+
+            if (string.IsNullOrEmpty(inputText))
+            {
+                MessageBox.Show(messageBoxText: "Source text of ITextEdit Manipulation for Insert is empty. Please input some text",
+                    caption: "No input");
+            }
+
+            if (!TryParseTextBoxToInt(textBox: ITextEditInputPositionDeleteTxtBox, textBoxValue: out int position))
+            {
+                MessageBox.Show(
+                messageBoxText: "Position int is null. Cannot continue",
+                caption: "No input");
+                return;
+            }
+
+            if (!TryParseTextBoxToInt(textBox: ITextEditInputLengthDeleteTxtBox, textBoxValue: out int length))
+            {
+                MessageBox.Show(
+                messageBoxText: "Length int is null. Cannot continue",
+                caption: "No input");
+                return;
+            }
+            if (!_textBuffer.EditInProgress)
+            {
+                _textEdit = _textBuffer.CreateEdit();
+            }
+            _textEdit.Delete(startPosition: position, charsToDelete: length);
+
+            PublishCurrentSnapshotAfterOperation();
+
+            _textOperationList.Add(new OperationData { Position = position, Length = length, Operation = TextOperation.Delete});
+
+            // ITextEditApply();
+            // AddItemToListView(startPosition: position, length, operationText: string.Empty, operation: "Delete");
+        }
+
+
+        private void ITextEditStartButton_Click(object sender, RoutedEventArgs e)
+        {
+            var inputText = ITextEditInputTextBox.Text;
+
+            if (string.IsNullOrEmpty(inputText))
+            {
+                MessageBox.Show(messageBoxText: "Source text of ITextEdit Manipulation is empty. Please input some text",
+                    caption: "No input");
+                return;
+            }
+
+            _textBuffer = _textBufferFactoryService
+
+            .CreateTextBuffer(inputText, _textBufferFactoryService.PlaintextContentType);
+
+            applyListView.Items.Clear();
+            //AddVersionAfterApplyClick(_textBuffer.CurrentSnapshot);
+        }
+
+        private StackPanel CreateStackPanelAndAddToListView()
+        {
+            var stackPanel = new StackPanel();
+            stackPanel.Orientation = Orientation.Vertical;
+            var stackBorder = new Border();
+            stackBorder.CornerRadius = new CornerRadius(5);
+
+            stackBorder.BorderThickness = new Thickness(5);
+            stackBorder.BorderBrush = Brushes.Red;
+            stackBorder.Child = stackPanel;
+            applyListView.Items.Add(stackBorder);
+            return stackPanel;
+        }      
+
+        private void ITextEditApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            ITextSnapshot textSnapshot;
+
+            try
+            {
+                textSnapshot = _textEdit.Apply();
+            }
+            catch (Exception exception)
+            {
+                if (exception is InvalidOperationException 
+                    && exception.Message == "Attempted to reuse an already applied edit.")
+                {
+                    MessageBox.Show(
+                    messageBoxText: $"{exception.Message} Looks like Apply is clicked. So attempt an opration such as Replace.",
+                    caption: "Invalid Operation");
+                    return;
+                }
+
+                if (exception is InvalidOperationException 
+                    && exception.Message == "Operation is not valid due to the current state of the object.")
+                {
+                    MessageBox.Show(
+                    messageBoxText: $"{exception.Message} Looks like Apply is clicked. So attempt an opration such as Replace.",
+                    caption: "Invalid Operation");
+                    return;
+                }
+
+                throw exception;
+            }
+            
+            AddVersionAfterApplyClick(textSnapshot);
+            PublishCurrentSnapshotAfterOperation();
+            _textOperationList.Clear();
+        }
+
+        private void AddVersionAfterApplyClick(ITextSnapshot textSnapshot)
+        {
+
+            var versionInfoString = string.Empty;
+
+            ITextVersion versionInfo;
+
+            if (_textEdit != null)
+            {
+                versionInfo = _textEdit.Snapshot.Version;
+            }
+            else
+            {
+                versionInfo = textSnapshot.Version;
+            }
+
+            versionInfoString = $"Version: {versionInfo.VersionNumber}";
+
+            var stackPanel = CreateStackPanelAndAddToListView();
+            var textBlock = new TextBlock();
+            textBlock.Text = versionInfoString;
+            stackPanel.Children.Add(textBlock);
+
+            AddOprationsToListView(stackPanel);
+
+            if (versionInfo.Changes == null)
+                return;
+
+            foreach (ITextChange change in _textEdit.Snapshot.Version.Changes)
+            {
+                textBlock = new TextBlock();
+                textBlock.Text = $"Old Text: {change.OldText} - New Text: {change.NewText}";
+                stackPanel.Children.Add(textBlock);
+            }
+        }
+
+        private void ITextEditResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            _textBuffer = null;
+            _textEdit = null;
+
+            ITextEditInputTextBox.Text = string.Empty;
+            //ITextEditInputSpanLengthTextBox.Text = string.Empty;
+            //ITextEditInputSpanStartTextBox.Text = string.Empty;
+            ITextEditInputPositionReplaceTxtBox.Text = string.Empty;
+            ITextEditInputLengthReplaceTxtBox.Text = string.Empty;
+            ITextEditInputTextReplaceTxtBox.Text = string.Empty;
+
+            ITextEditOutputTextBlock.Text = string.Empty;
+            //readonlyExtentsTextBlock.Text = string.Empty;
+
+
+            applyListView.Items.Clear();
+        }
+
+        private void PublishCurrentSnapshotAfterOperation()
+        {
+            var sn2 = _textBuffer.CurrentSnapshot;
+
+            ITextEditOutputTextBlock.Text = sn2.GetText();
+        }
+
+        private void AddOprationsToListView(StackPanel stackPanel)
+        {
+            foreach (var textOperation in _textOperationList)
+            {
+                var textBlock = new TextBlock();
+                textBlock.Foreground = Brushes.Green;
+                var itemString = string.Empty;
+                if (textOperation.Operation == TextOperation.Replace)
+                {
+                    itemString = $"Opration: {textOperation.Operation}, start: {textOperation.Position}, length: {textOperation.Length}. Replace text: {textOperation.OperationText}";
+                }
+                else if (textOperation.Operation == TextOperation.Delete)
+                {
+                    itemString = $"Opration: {textOperation.Operation}, start: {textOperation.Position}, length: {textOperation.Length}";
+                }
+                else if (textOperation.Operation == TextOperation.Insert)
+                {
+                    itemString = $"Opration: {textOperation.Operation}, start: {textOperation.Position}, Insert text: {textOperation.OperationText}";
+                }
+                else
+                {
+                    MessageBox.Show($"Unknown operation: {textOperation.Operation}", "Invalid operation", MessageBoxButton.OK, MessageBoxImage.Information);
+                    System.Diagnostics.Debugger.Break();
+                }
+
+                textBlock.Text = itemString;
+                stackPanel.Children.Add(textBlock);
+            }
+        }
+
+        private bool TryParseTextBoxToInt(TextBox textBox, out int textBoxValue)
+        {
+            textBoxValue = 0;
+
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                MessageBox.Show(
+                    messageBoxText: $"{textBox.Tag} is empty. Please input some text",
+                    caption: "No input");
+                return false;
+            }
+
+            return int.TryParse(textBox.Text, out textBoxValue);
+        }
+
+        private bool IsTextBufferNull()
+        {
+            if (_textBuffer == null)
+            {
+                MessageBox.Show(
+                messageBoxText: "Text buffer is null, click start button",
+                caption: "Click start");
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ src/tasks/221620-TextVersionIntro/ReadMe.md
@@ -0,0 +1,41 @@
+## Objective
+
+1. Introduces IReadOnlyRegionEdit
+
+## Build and Run
+
+1. Reset Visual Studio Exp instance and then Launch it.
+
+![Reset Visual Studio Exp](../200500-VSixBlankProjectAnalysis/images/57_50_ResetVsExpIntance.jpg)
+
+1. View -> Other Windows -> Look for ReadOnlyEditToolWindow
+
+2. Enter text say `01234567890123456789` 20 chars in the top text box. Click the `Get Read Only Extents` button. You should now see 0 read only extents.
+
+![Get Readonly Extents](Images/50_50_GetReadOnlyExents.png)
+
+3. Then click the Start button. Then put 0 and lenght 10 and click `Create ReadOnly Region` button. 
+
+![Create Read Only Extent](Images/51_50_CreateReadOnlyRegion.png)
+
+4. Now try to edit by replacing some text, whose span overlaps with the Readonly region. So put the position as say 7 and lenght say 5, and replace text say ABC, click `Replace` and then click `Apply`. Notice that there is no change to the text. This is because the edit overlaps with ReadOnly region. Read only regions do not allow edits.
+
+![Replace in read only regions](Images/52_50_ReplaceInReadonlyRegion.png)
+
+5. Now try an edit in 11 till 20 space. 
+
+![Edit in editable space](Images/53_50_EditInEditableSpace.png)
+
+6. Reset, and start over again. This time, create three readonly regions of which, the second and third overlap as follows. So although there are three regions, only two are readonly regions.
+
+![Three Read only regions](Images/54_50_OnlyTwoReadonlyRegions.png)
+
+1. 
+
+## Reference.
+
+1. https://mihailromanov.wordpress.com/2021/11/05/json-on-steroids-2-2-visual-studio-editor-itextbuffer-and-related-types
+
+2. https://learn.microsoft.com/en-us/visualstudio/extensibility/inside-the-editor#itextedit-textversion-and-text-change-notifications
+
+3. Here we go...
