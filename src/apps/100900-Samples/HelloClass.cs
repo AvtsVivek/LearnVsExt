cb0e22a736110e0fc944c96f76b422dba0774660
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2024-12-28 19:55:51 +0530

    Added more info

---
+++ src/apps/100900-Samples/HelloClass.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class Class1
+{
+    public Class1()
+    {
+
+        /*
+
+        Hello
+
+        Hellpo
+
+        Hello
+
+        Hello
+
+        Hello
+
+
+        */
+
+    }
+
+    public static void Main(string[] args)
+    {
+
+    }
+}
